fb83af1f36dcd62b587d99c352c1d1f76a138aee
Thomas Laporte <thomas.laporte@hotmail.com>
2022-06-01 22:35:29 -0400

    asdffff

--- Prog3-TP2-Avions/Vue/GenerateurVue.Designer.cs
+++ Prog3-TP2-Avions/Vue/GenerateurVue.Designer.cs
@@ -29,12 +29,375 @@ namespace Prog3_TP2_Avions.Vue
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lstAero = new System.Windows.Forms.ListBox();
+            this.txtNomAero = new System.Windows.Forms.TextBox();
+            this.txtMinPass = new System.Windows.Forms.TextBox();
+            this.txtMaxPass = new System.Windows.Forms.TextBox();
+            this.txtMinMarch = new System.Windows.Forms.TextBox();
+            this.txtMaxMarch = new System.Windows.Forms.TextBox();
+            this.btnPosition = new System.Windows.Forms.Button();
+            this.lblNomAero = new System.Windows.Forms.Label();
+            this.lblPosition = new System.Windows.Forms.Label();
+            this.lblMinPass = new System.Windows.Forms.Label();
+            this.lblMaxPass = new System.Windows.Forms.Label();
+            this.lblMinMarch = new System.Windows.Forms.Label();
+            this.lblMaxMarch = new System.Windows.Forms.Label();
+            this.lblInfosAero = new System.Windows.Forms.Label();
+            this.btnAjoutAero = new System.Windows.Forms.Button();
+            this.lstVehi = new System.Windows.Forms.ListBox();
+            this.lblInfosVehi = new System.Windows.Forms.Label();
+            this.lblEntretient = new System.Windows.Forms.Label();
+            this.lblDebarquement = new System.Windows.Forms.Label();
+            this.lblEmbarquement = new System.Windows.Forms.Label();
+            this.lblVitesse = new System.Windows.Forms.Label();
+            this.lblNomVehi = new System.Windows.Forms.Label();
+            this.txtEntretient = new System.Windows.Forms.TextBox();
+            this.txtDebarquement = new System.Windows.Forms.TextBox();
+            this.txtEmbarquement = new System.Windows.Forms.TextBox();
+            this.txtVitesse = new System.Windows.Forms.TextBox();
+            this.txtNomVehi = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.btnAjoutAvion = new System.Windows.Forms.Button();
+            this.btnAjoutScenario = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lstAero
+            // 
+            this.lstAero.FormattingEnabled = true;
+            this.lstAero.Location = new System.Drawing.Point(15, 12);
+            this.lstAero.Name = "lstAero";
+            this.lstAero.Size = new System.Drawing.Size(1073, 121);
+            this.lstAero.TabIndex = 0;
+            // 
+            // txtNomAero
+            // 
+            this.txtNomAero.Location = new System.Drawing.Point(53, 142);
+            this.txtNomAero.Name = "txtNomAero";
+            this.txtNomAero.Size = new System.Drawing.Size(100, 20);
+            this.txtNomAero.TabIndex = 1;
+            // 
+            // txtMinPass
+            // 
+            this.txtMinPass.Location = new System.Drawing.Point(472, 142);
+            this.txtMinPass.Name = "txtMinPass";
+            this.txtMinPass.Size = new System.Drawing.Size(44, 20);
+            this.txtMinPass.TabIndex = 3;
+            // 
+            // txtMaxPass
+            // 
+            this.txtMaxPass.Location = new System.Drawing.Point(654, 142);
+            this.txtMaxPass.Name = "txtMaxPass";
+            this.txtMaxPass.Size = new System.Drawing.Size(44, 20);
+            this.txtMaxPass.TabIndex = 4;
+            // 
+            // txtMinMarch
+            // 
+            this.txtMinMarch.Location = new System.Drawing.Point(849, 142);
+            this.txtMinMarch.Name = "txtMinMarch";
+            this.txtMinMarch.Size = new System.Drawing.Size(44, 20);
+            this.txtMinMarch.TabIndex = 5;
+            // 
+            // txtMaxMarch
+            // 
+            this.txtMaxMarch.Location = new System.Drawing.Point(1043, 143);
+            this.txtMaxMarch.Name = "txtMaxMarch";
+            this.txtMaxMarch.Size = new System.Drawing.Size(44, 20);
+            this.txtMaxMarch.TabIndex = 6;
+            // 
+            // btnPosition
+            // 
+            this.btnPosition.BackColor = System.Drawing.Color.Red;
+            this.btnPosition.Location = new System.Drawing.Point(270, 141);
+            this.btnPosition.Name = "btnPosition";
+            this.btnPosition.Size = new System.Drawing.Size(75, 23);
+            this.btnPosition.TabIndex = 7;
+            this.btnPosition.Text = "Position";
+            this.btnPosition.UseVisualStyleBackColor = false;
+            // 
+            // lblNomAero
+            // 
+            this.lblNomAero.AutoSize = true;
+            this.lblNomAero.Location = new System.Drawing.Point(12, 147);
+            this.lblNomAero.Name = "lblNomAero";
+            this.lblNomAero.Size = new System.Drawing.Size(35, 13);
+            this.lblNomAero.TabIndex = 8;
+            this.lblNomAero.Text = "Nom: ";
+            // 
+            // lblPosition
+            // 
+            this.lblPosition.AutoSize = true;
+            this.lblPosition.Location = new System.Drawing.Point(171, 146);
+            this.lblPosition.Name = "lblPosition";
+            this.lblPosition.Size = new System.Drawing.Size(93, 13);
+            this.lblPosition.TabIndex = 9;
+            this.lblPosition.Text = "Position (À choisir)";
+            // 
+            // lblMinPass
+            // 
+            this.lblMinPass.AutoSize = true;
+            this.lblMinPass.Location = new System.Drawing.Point(367, 147);
+            this.lblMinPass.Name = "lblMinPass";
+            this.lblMinPass.Size = new System.Drawing.Size(102, 13);
+            this.lblMinPass.TabIndex = 10;
+            this.lblMinPass.Text = "Minimum passagers:";
+            // 
+            // lblMaxPass
+            // 
+            this.lblMaxPass.AutoSize = true;
+            this.lblMaxPass.Location = new System.Drawing.Point(546, 145);
+            this.lblMaxPass.Name = "lblMaxPass";
+            this.lblMaxPass.Size = new System.Drawing.Size(105, 13);
+            this.lblMaxPass.TabIndex = 11;
+            this.lblMaxPass.Text = "Maximum passagers:";
+            // 
+            // lblMinMarch
+            // 
+            this.lblMinMarch.AutoSize = true;
+            this.lblMinMarch.Location = new System.Drawing.Point(727, 145);
+            this.lblMinMarch.Name = "lblMinMarch";
+            this.lblMinMarch.Size = new System.Drawing.Size(119, 13);
+            this.lblMinMarch.TabIndex = 12;
+            this.lblMinMarch.Text = "Minimum marchandises:";
+            // 
+            // lblMaxMarch
+            // 
+            this.lblMaxMarch.AutoSize = true;
+            this.lblMaxMarch.Location = new System.Drawing.Point(918, 146);
+            this.lblMaxMarch.Name = "lblMaxMarch";
+            this.lblMaxMarch.Size = new System.Drawing.Size(122, 13);
+            this.lblMaxMarch.TabIndex = 13;
+            this.lblMaxMarch.Text = "Maximum marchandises:";
+            // 
+            // lblInfosAero
+            // 
+            this.lblInfosAero.AutoSize = true;
+            this.lblInfosAero.Location = new System.Drawing.Point(491, 165);
+            this.lblInfosAero.Name = "lblInfosAero";
+            this.lblInfosAero.Size = new System.Drawing.Size(484, 13);
+            this.lblInfosAero.TabIndex = 15;
+            this.lblInfosAero.Text = "Les minimums et maximums indique le nombre de groupes de passagers et de marchand" +
+    "ises à l\'heure.";
+            // 
+            // btnAjoutAero
+            // 
+            this.btnAjoutAero.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btnAjoutAero.Location = new System.Drawing.Point(15, 185);
+            this.btnAjoutAero.Name = "btnAjoutAero";
+            this.btnAjoutAero.Size = new System.Drawing.Size(1073, 20);
+            this.btnAjoutAero.TabIndex = 16;
+            this.btnAjoutAero.Text = "Ajouter l\'aéroport";
+            this.btnAjoutAero.UseVisualStyleBackColor = false;
+            // 
+            // lstVehi
+            // 
+            this.lstVehi.FormattingEnabled = true;
+            this.lstVehi.Location = new System.Drawing.Point(15, 223);
+            this.lstVehi.Name = "lstVehi";
+            this.lstVehi.Size = new System.Drawing.Size(1073, 121);
+            this.lstVehi.TabIndex = 17;
+            // 
+            // lblInfosVehi
+            // 
+            this.lblInfosVehi.AutoSize = true;
+            this.lblInfosVehi.Location = new System.Drawing.Point(738, 376);
+            this.lblInfosVehi.Name = "lblInfosVehi";
+            this.lblInfosVehi.Size = new System.Drawing.Size(288, 13);
+            this.lblInfosVehi.TabIndex = 28;
+            this.lblInfosVehi.Text = "C\'est le temps que prends l\'avion à faire l\'action (en heures).";
+            // 
+            // lblEntretient
+            // 
+            this.lblEntretient.AutoSize = true;
+            this.lblEntretient.Location = new System.Drawing.Point(982, 357);
+            this.lblEntretient.Name = "lblEntretient";
+            this.lblEntretient.Size = new System.Drawing.Size(55, 13);
+            this.lblEntretient.TabIndex = 27;
+            this.lblEntretient.Text = "Entretient:";
+            // 
+            // lblDebarquement
+            // 
+            this.lblDebarquement.AutoSize = true;
+            this.lblDebarquement.Location = new System.Drawing.Point(825, 358);
+            this.lblDebarquement.Name = "lblDebarquement";
+            this.lblDebarquement.Size = new System.Drawing.Size(80, 13);
+            this.lblDebarquement.TabIndex = 26;
+            this.lblDebarquement.Text = "Débarquement:";
+            // 
+            // lblEmbarquement
+            // 
+            this.lblEmbarquement.AutoSize = true;
+            this.lblEmbarquement.Location = new System.Drawing.Point(664, 358);
+            this.lblEmbarquement.Name = "lblEmbarquement";
+            this.lblEmbarquement.Size = new System.Drawing.Size(81, 13);
+            this.lblEmbarquement.TabIndex = 25;
+            this.lblEmbarquement.Text = "Embarquement:";
+            // 
+            // lblVitesse
+            // 
+            this.lblVitesse.AutoSize = true;
+            this.lblVitesse.Location = new System.Drawing.Point(488, 358);
+            this.lblVitesse.Name = "lblVitesse";
+            this.lblVitesse.Size = new System.Drawing.Size(44, 13);
+            this.lblVitesse.TabIndex = 24;
+            this.lblVitesse.Text = "Vitesse:";
+            // 
+            // lblNomVehi
+            // 
+            this.lblNomVehi.AutoSize = true;
+            this.lblNomVehi.Location = new System.Drawing.Point(12, 358);
+            this.lblNomVehi.Name = "lblNomVehi";
+            this.lblNomVehi.Size = new System.Drawing.Size(35, 13);
+            this.lblNomVehi.TabIndex = 23;
+            this.lblNomVehi.Text = "Nom: ";
+            // 
+            // txtEntretient
+            // 
+            this.txtEntretient.Location = new System.Drawing.Point(1043, 353);
+            this.txtEntretient.Name = "txtEntretient";
+            this.txtEntretient.Size = new System.Drawing.Size(44, 20);
+            this.txtEntretient.TabIndex = 22;
+            // 
+            // txtDebarquement
+            // 
+            this.txtDebarquement.Location = new System.Drawing.Point(911, 353);
+            this.txtDebarquement.Name = "txtDebarquement";
+            this.txtDebarquement.Size = new System.Drawing.Size(44, 20);
+            this.txtDebarquement.TabIndex = 21;
+            // 
+            // txtEmbarquement
+            // 
+            this.txtEmbarquement.Location = new System.Drawing.Point(751, 353);
+            this.txtEmbarquement.Name = "txtEmbarquement";
+            this.txtEmbarquement.Size = new System.Drawing.Size(44, 20);
+            this.txtEmbarquement.TabIndex = 20;
+            // 
+            // txtVitesse
+            // 
+            this.txtVitesse.Location = new System.Drawing.Point(538, 353);
+            this.txtVitesse.Name = "txtVitesse";
+            this.txtVitesse.Size = new System.Drawing.Size(44, 20);
+            this.txtVitesse.TabIndex = 19;
+            // 
+            // txtNomVehi
+            // 
+            this.txtNomVehi.Location = new System.Drawing.Point(53, 353);
+            this.txtNomVehi.Name = "txtNomVehi";
+            this.txtNomVehi.Size = new System.Drawing.Size(139, 20);
+            this.txtNomVehi.TabIndex = 18;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(230, 356);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 29;
+            this.label1.Text = "Type:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(270, 352);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(142, 21);
+            this.comboBox1.TabIndex = 30;
+            // 
+            // btnAjoutAvion
+            // 
+            this.btnAjoutAvion.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btnAjoutAvion.Location = new System.Drawing.Point(15, 401);
+            this.btnAjoutAvion.Name = "btnAjoutAvion";
+            this.btnAjoutAvion.Size = new System.Drawing.Size(1073, 20);
+            this.btnAjoutAvion.TabIndex = 31;
+            this.btnAjoutAvion.Text = "Ajouter l\'avion";
+            this.btnAjoutAvion.UseVisualStyleBackColor = false;
+            // 
+            // btnAjoutScenario
+            // 
+            this.btnAjoutScenario.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btnAjoutScenario.Location = new System.Drawing.Point(15, 442);
+            this.btnAjoutScenario.Name = "btnAjoutScenario";
+            this.btnAjoutScenario.Size = new System.Drawing.Size(1073, 36);
+            this.btnAjoutScenario.TabIndex = 32;
+            this.btnAjoutScenario.Text = "Ajouter l\'avion";
+            this.btnAjoutScenario.UseVisualStyleBackColor = false;
+            // 
+            // GenerateurVue
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "GenerateurVue";
+            this.ClientSize = new System.Drawing.Size(1102, 500);
+            this.Controls.Add(this.btnAjoutScenario);
+            this.Controls.Add(this.btnAjoutAvion);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblInfosVehi);
+            this.Controls.Add(this.lblEntretient);
+            this.Controls.Add(this.lblDebarquement);
+            this.Controls.Add(this.lblEmbarquement);
+            this.Controls.Add(this.lblVitesse);
+            this.Controls.Add(this.lblNomVehi);
+            this.Controls.Add(this.txtEntretient);
+            this.Controls.Add(this.txtDebarquement);
+            this.Controls.Add(this.txtEmbarquement);
+            this.Controls.Add(this.txtVitesse);
+            this.Controls.Add(this.txtNomVehi);
+            this.Controls.Add(this.lstVehi);
+            this.Controls.Add(this.btnAjoutAero);
+            this.Controls.Add(this.lblInfosAero);
+            this.Controls.Add(this.lblMaxMarch);
+            this.Controls.Add(this.lblMinMarch);
+            this.Controls.Add(this.lblMaxPass);
+            this.Controls.Add(this.lblMinPass);
+            this.Controls.Add(this.lblPosition);
+            this.Controls.Add(this.lblNomAero);
+            this.Controls.Add(this.btnPosition);
+            this.Controls.Add(this.txtMaxMarch);
+            this.Controls.Add(this.txtMinMarch);
+            this.Controls.Add(this.txtMaxPass);
+            this.Controls.Add(this.txtMinPass);
+            this.Controls.Add(this.txtNomAero);
+            this.Controls.Add(this.lstAero);
+            this.Name = "GenerateurVue";
+            this.Text = "Générateur de scénarios";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox lstAero;
+        private System.Windows.Forms.TextBox txtNomAero;
+        private System.Windows.Forms.TextBox txtMinPass;
+        private System.Windows.Forms.TextBox txtMaxPass;
+        private System.Windows.Forms.TextBox txtMinMarch;
+        private System.Windows.Forms.TextBox txtMaxMarch;
+        private System.Windows.Forms.Button btnPosition;
+        private System.Windows.Forms.Label lblNomAero;
+        private System.Windows.Forms.Label lblPosition;
+        private System.Windows.Forms.Label lblMinPass;
+        private System.Windows.Forms.Label lblMaxPass;
+        private System.Windows.Forms.Label lblMinMarch;
+        private System.Windows.Forms.Label lblMaxMarch;
+        private System.Windows.Forms.Label lblInfosAero;
+        private System.Windows.Forms.Button btnAjoutAero;
+        private System.Windows.Forms.ListBox lstVehi;
+        private System.Windows.Forms.Label lblInfosVehi;
+        private System.Windows.Forms.Label lblEntretient;
+        private System.Windows.Forms.Label lblDebarquement;
+        private System.Windows.Forms.Label lblEmbarquement;
+        private System.Windows.Forms.Label lblVitesse;
+        private System.Windows.Forms.Label lblNomVehi;
+        private System.Windows.Forms.TextBox txtEntretient;
+        private System.Windows.Forms.TextBox txtDebarquement;
+        private System.Windows.Forms.TextBox txtEmbarquement;
+        private System.Windows.Forms.TextBox txtVitesse;
+        private System.Windows.Forms.TextBox txtNomVehi;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button btnAjoutAvion;
+        private System.Windows.Forms.Button btnAjoutScenario;
     }
 }
